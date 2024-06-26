4e7ddd50b4899f28e62cf6e22c1a8e59d079806e
2812SAM <124618164+2812SAM@users.noreply.github.com>
2024-06-26 10:02:13 +0000

    data types

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -15,3 +15,33 @@ let bool = false                    // Boolean data type
  console.table([typeof sadness,typeof name ,typeof rollNo ,typeof future ,typeof null ,typeof symbol ,typeof bool]);                    
 
  // Type of Null is Object , Type of Undefined id undefined
+
+ // Two typs of data types
+ //1) primitive ---- 7 data types:- number ,big number ,boolean , null , undefined , string , symbol.
+
+ let Id = Symbol('123')
+ let otherId = Symbol('123')
+
+ // both are different .
+console.log(Id===otherId);
+
+
+ //***************** type of BIG INT is?  -----> Object
+
+ // 2) Non primitive ----- Arrays , Objects , function
+ // which can be passed as reference.
+ const hero = ["spiderman","shaktiman","angela"]
+ let myObj = {
+    name: "swayam",
+    age: 20,
+ }
+
+ const myFunction = function(){
+    console.log("hello world")
+ }
+
+ console.table([typeof hero ,typeof myObj , typeof myFunction])
+
+ //array type-----> Object
+ //object type-----> object
+ //function type -------> functionObject
