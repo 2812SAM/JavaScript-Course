695cebd649bfa118fd3a4d4943c2ea50854b4684
2812SAM <124618164+2812SAM@users.noreply.github.com>
2024-06-26 06:35:26 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,21 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 <= 1);
+console.log(2 == 1);
+
+console.log("2" > 1);
+console.log("2" <= 1);
+console.log("2" == 0);
+
+
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null < 0);
+
+
+// == works differently as compared to > , < , >= , <=;
+// In case of <= , > ,< i.e. comparisons operations the String is converted into NUMber where as in == there is no change.
+
+// === (strict check)---- it aslo checks the both variables datatypes which are getting compared.
+console.log(null === 0);
