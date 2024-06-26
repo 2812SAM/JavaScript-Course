4917be5bab6743afe2a4724a8866be3ff5a612fc
2812SAM <124618164+2812SAM@users.noreply.github.com>
2024-06-26 05:43:51 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accId = 12345;
+
+// You can not change cont values after defining it once;
+
+let name = "swayam";
+var rollNo = "22103149";
+
+// Never use VAR data type becasue it has scope constarints. cant diffrentiate between scopes.
+
+let income;
+
+console.log(name);
+
+console.table([name,rollNo,accId,income]);
+
+// IN order to print multiple values together use console.table([])
+
+//NO NEED OF ; AT THE END OF LINE
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+//many types of data types are there in java script
+let name="Swayam"   // Strings Datatype
+let rollNo=22103149 // number  range of number is from 0 to -(2^53-1) to (2^53-1)
+                    // to store more large numbers use BigInt is used
+let future          // Undefined data type
+let sadness=null    // Null data type
+let bool = false                    // Boolean data type
+                    // Symbol => used for uniqueness , most used in figma and react
+
+
+                    // The above were primitive data types
+                    // object => user defined data type;
+ // How to check the type of any variable ?
+ // Use consol.log(typeof variable name)
+ console.table([typeof sadness,typeof name ,typeof rollNo ,typeof future ,typeof null ,typeof symbol ,typeof bool]);                    
+
+ // Type of Null is Object , Type of Undefined id undefined
